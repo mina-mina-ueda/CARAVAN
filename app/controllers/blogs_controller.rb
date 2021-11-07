f449e44103050ce786a2bb2439a1a2ff6bef32e0
mina-mina-ueda <91769651+mina-mina-ueda@users.noreply.github.com>
2021-11-07 13:58:44 +0000

    [Start]4-8

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,13 +1,10 @@
 class BlogsController < ApplicationController
   def index
+    @blogs = Blog.all
   end
 
   def show
     @blog = Blog.find(params[:id])
-  end
-
-  def index
-    @blogs =Blog.all
   end
 
   def new
@@ -15,9 +12,12 @@ class BlogsController < ApplicationController
   end
 
   def create
-    blog = Blog.new(blog_params)
-    blog.save
-    redirect_to blogs_path(blog.id)
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blogs_path(@blog.id)
+    else
+      render :new
+    end
   end
   
   def edit
@@ -30,9 +30,14 @@ class BlogsController < ApplicationController
     redirect_to blog_path(blog)
   end
 
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
+  end
+
   private
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
   end
-
 end
